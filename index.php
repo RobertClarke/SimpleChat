e4d76798fcad50502598cacb4f56b73d730f13f6
RobertClarke <robert@rjfc.net>
2013-07-07 00:54:31 -0700

    Hey zewl

---
+++ index.php
@@ -0,0 +1,9 @@
+Hey Zewl.
+
+Pretty awesome shit that I've been testing, but basically, go grab the Github client, clone the SimpleChat project that I've invited you to, add all the current files and stuff, and they'll be dropped into /public_html of the cPanel account I setup for you when you commit new changes.
+
+Means that you can have the codebase all on Github, and then not worry about new changes being pushed out quickly, because this setup is VERY quick.
+
+ - R
+
+<?php echo "mong m8" ;?>
